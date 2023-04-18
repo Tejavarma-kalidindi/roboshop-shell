ea411b714917cf2552b658110b8eb40ef052df80
Tejavarma Kalidindi (publicissapient.com) <tejavarma.kalidindi@publicissapient.com>
2023-04-18 12:24:15 +0530

    Frontend service setup

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,11 @@
-yum install nginx -y 
-systemctl enable nginx 
-systemctl start nginx 
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+yum install nginx -y
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-systemctl restart nginx 
+
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
+
+systemctl enable nginx
+systemctl restart nginx
