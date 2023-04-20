1dc6762aeab4102c8e336e6a2435809c6b5293c0
Tejavarma Kalidindi (publicissapient.com) <tejavarma.kalidindi@publicissapient.com>
2023-04-20 11:12:32 +0530

    user setup

---
+++ user.sh
@@ -0,0 +1,21 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
+cd /app 
+unzip /tmp/user.zip
+npm install 
+
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+systemctl daemon-reload
+systemctl enable user 
+systemctl start user
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.tejadevops.online </app/schema/user.js
