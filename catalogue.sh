58b64d32dd13382a6ca396994be668c8048b5e0b
Tejavarma Kalidindi (publicissapient.com) <tejavarma.kalidindi@publicissapient.com>
2023-04-20 10:40:34 +0530

    Catalogue setup

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,14 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
+
+rm -rf /app
 mkdir /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
 npm install 
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
