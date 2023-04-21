1187b545ea9dc7074593febf991a9417de9cd3be
Tejavarma Kalidindi (publicissapient.com) <tejavarma.kalidindi@publicissapient.com>
2023-04-21 10:29:36 +0530

    frontend echo update

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,19 @@
+echo -e "\e[31m>>>>>>>>>> Install Nginx <<<<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[31m>>>>>>>>>> Downlaod Application <<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[31m>>>>>>>>>> Remove Default Nginx Code <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[31m>>>>>>>>>> Extract App <<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
+echo -e "\e[31m>>>>>>>>>> Copy roboshop Config file <<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
+echo -e "\e[31m>>>>>>>>>> Start Nginx <<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl restart nginx
